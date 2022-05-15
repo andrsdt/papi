2eeab77eeffb1047bee03305b9fe4b0d6995f01e
Andres Duran <andresdt@pm.me>
2022-05-15 22:00:17 +0200

    feat: add rate limit, update deps

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import bodyParser from "body-parser";
+import rateLimit from "express-rate-limit";
 import config from "./config.json";
 import { generatePassword } from "./utils/passwordGenerator";
 
@@ -21,6 +22,17 @@ app.use(
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// rate limit
+const limiter = rateLimit({
+	windowMs: 60 * 60 * 1000, // 1 hour
+	max: 100, // Limit each IP to 100 requests per `window` (here, per hour)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter);
 
 app.get("/api/", (req, res) => res.send(generatePassword(req)));
 
