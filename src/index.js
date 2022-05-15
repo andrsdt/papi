45c58bad96c8d5d022a9cf9bd2b5a6e51e218398
Andres Duran <andresdt@pm.me>
2022-05-15 21:06:16 +0200

    fix: support for heroku

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import bodyParser from "body-parser";
-import api from "./api";
 import config from "./config.json";
 import { generatePassword } from "./utils/passwordGenerator";
 
@@ -23,10 +22,7 @@ app.use(
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// api router
-app.use("/api", api({ config }));
-
-api.get("/", (req, res) => res.send(generatePassword(req)));
+app.get("/api/", (req, res) => res.send(generatePassword(req)));
 
 app.server.listen(process.env.PORT || config.port, () => {
 	console.log(`Started on port ${app.server.address().port}`);
